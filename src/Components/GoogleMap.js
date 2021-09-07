b0fa71685ba5b272c85c8546a4acc9cba67cd7d8
aakashgovardhane <aakashgovardhane@gmail.com>
2021-09-08 01:28:25 +0530

    google-map

--- src/Components/GoogleMap.js
+++ src/Components/GoogleMap.js
@@ -8,37 +8,52 @@ class GoogleMaps extends React.Component {
         super();
         this.state = {
             center: { lat: 19.993982, lng: 73.790416 },
-            poly:[],
+            poly: [],
         }
     }
 
-    initMap = (mapProps, map) => {
-        let poly = this.state;
-        let poly1 = [];
+    initMap = async (mapProps, map) => {
+        let poly = [];
         const { google } = mapProps;
-        this.initializePoly = (poly) => {
-            poly = new google.maps.Polyline({
+
+
+        // const marker = new google.maps.Marker({
+        //     position: { lat: 19.997454, lng: 73.789803 },
+        //     map: map
+        // })
+
+        // const infoWindow = new google.maps.InfoWindow({
+        //     content: "<h1>Aakash</h1>"
+        // })
+        // marker.addListener("click", () => {
+        //     infoWindow.open(map, marker);
+        // })
+
+        const addMarker = (markerData) => {
+            const marker = new google.maps.Marker({
+                position: markerData.coords,
+                map: map
+            })
+            poly.push({ lat:markerData.coords.lat,lng:markerData.coords.lng});
+
+            console.log(poly);
+            const drawPoly = new google.maps.Polygon({
+                paths:poly,
                 strokeColor: "#FF0000",
                 strokeOpacity: 0.8,
                 strokeWeight: 2,
                 fillColor: "#FF0000",
                 fillOpacity: 0.35,
             });
-            poly.setMap(map);
-        };
-        this.addCircle = async (location) => {
-            this.setState({poly:location});
+            drawPoly.setMap(map);
+
         }
         google.maps.event.addListener(map, "click", (event) => {
-            let lastWayPoint = event.latLng;
-            this.setState((state) => ({
-                ...state,
-                lastWaypointLocation: lastWayPoint,
-            }));
-            console.log("addcircle called by map click event",event.latLng);
+            addMarker({ coords: {lat:event.latLng.lat(),lng:event.latLng.lng()}})
+        })
+    }
+    componentDidMount() {
 
-            this.addCircle(event.latLng);
-        });
     }
     render() {
         return (
